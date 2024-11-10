8fcc74f1fc4850651c6ad8b739d13e739621c40b
IsseCy <simtecapplications@web.de>
2024-11-10 16:35:55 +0100

    completed ui and added readme.md

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@
 //? Navigation button logic
 const homeSection = document.getElementById('main-section-container');
 const addPbSection = document.getElementById('addPb-section-container');
+const profileSection = document.getElementById('profile-section-container');
 
 //* home tab
 const returnToHome = document.querySelectorAll('.returnToHome');
@@ -12,10 +13,11 @@ for (button of returnToHome) {
         document.getElementById('return-button').style.visibility = "hidden";
         homeSection.style.display = "flex";
         addPbSection.style.display = "none";
+        profileSection.style.display = "none";
     });
 }
 
-//* add PB section
+//* add PB tab
 const addPbButtons = document.querySelectorAll('.addPbButton');
 const returnButton = document.getElementById('return-button');
 
@@ -24,5 +26,16 @@ for (button of addPbButtons) {
         addPbSection.style.display = "flex";
         returnButton.style.visibility = "visible";
         homeSection.style.display = "none";
+        profileSection.style.display = "none";
     });
 }
+
+//* profile tab
+const profileButton = document.getElementById('nav-profile-section-button');
+
+profileButton.addEventListener('click', () => {
+    profileSection.style.display = "flex";
+    returnButton.style.visibility = "visible";
+    homeSection.style.display = "none";
+    addPbSection.style.display = "none";
+});
